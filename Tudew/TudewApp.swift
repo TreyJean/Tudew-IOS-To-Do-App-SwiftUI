5f99df0d17ab23c57c4a6cd5c8885d4f8e640a27
TreyJean <treyjean127@gmail.com>
2021-06-24 19:50:08 -0400

    Initial Commit

---
+++ Tudew/TudewApp.swift
@@ -0,0 +1,17 @@
+//
+//  TudewApp.swift
+//  Tudew
+//
+//  Created by Trey Jean-Baptiste on 6/24/21.
+//
+
+import SwiftUI
+
+@main
+struct TudewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
