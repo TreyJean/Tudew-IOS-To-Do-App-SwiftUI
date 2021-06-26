9da7bf68f005120f10d8f102d0656d8ebcfa67ae
TreyJean <treyjean127@gmail.com>
2021-06-26 19:17:54 -0400

    Tudew Version: 1.0 - Commit # 01 	- Issues 		• Launch Screen not updating with new images 		• Launch Screen image blurry (replace with SVG) 	- Fixes 		• None

--- Tudew/ContentView.swift
+++ Tudew/ContentView.swift
@@ -8,9 +8,167 @@
 import SwiftUI
 
 struct ContentView: View {
+    @Environment(\.managedObjectContext) var managedObjectContext
+    
+    @FetchRequest(entity: Item.entity(), sortDescriptors: [NSSortDescriptor(keyPath: \Item.created_at, ascending: true)], predicate: NSPredicate(format: "priority == %@", "low")) var low_pri_items: FetchedResults<Item>
+    
+    @FetchRequest(entity: Item.entity(), sortDescriptors: [NSSortDescriptor(keyPath: \Item.created_at, ascending: true)], predicate: NSPredicate(format: "priority == %@", "normal")) var norm_pri_items: FetchedResults<Item>
+    
+    @FetchRequest(entity: Item.entity(), sortDescriptors: [NSSortDescriptor(keyPath: \Item.created_at, ascending: true)], predicate: NSPredicate(format: "priority == %@", "high")) var high_pri_items: FetchedResults<Item>
+    
+    
+    let dateFormatter = DateFormatter()
+    
+    func setup() {
+        dateFormatter.dateStyle = .medium
+        
+        // US English Locale (en_US)
+        dateFormatter.locale = Locale(identifier: "en_US")
+    }
+    
+    func removeLowItem(at offsets: IndexSet) {
+        for index in offsets {
+            let item = low_pri_items[index]
+            PersistenceController.shared.delete(item)
+        }
+    }
+    
+    func removeNormItem(at offsets: IndexSet) {
+        for index in offsets {
+            let item = norm_pri_items[index]
+            PersistenceController.shared.delete(item)
+        }
+    }
+    
+    func removeHighItem(at offsets: IndexSet) {
+        for index in offsets {
+            let item = high_pri_items[index]
+            PersistenceController.shared.delete(item)
+        }
+    }
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            ZStack {
+                VStack {
+                    List {
+                        Section(header: Text("High Priority")) {
+                            if high_pri_items.isEmpty {
+                                Text("No High Priority Items.")
+                                    .foregroundColor(.secondary)
+                            }
+                            
+                            ForEach(high_pri_items, id: \.self) { item in
+                                VStack(alignment: .leading) {
+                                    Text("\(item.title ?? "Unknown")")
+                                    Text(dateFormatter.string(from: item.created_at!))
+                                        .font(.subheadline)
+                                        .fontWeight(.light)
+                                }
+                            }.onDelete(perform: removeHighItem)
+                        }
+                        
+                        Section(header: Text("Normal Priority")) {
+                            if norm_pri_items.isEmpty {
+                                Text("No Normal Priority Items.")
+                                    .foregroundColor(.secondary)
+                            }
+                            
+                            ForEach(norm_pri_items, id: \.self) { item in
+                                VStack(alignment: .leading) {
+                                    Text("\(item.title ?? "Unknown")")
+                                    Text(dateFormatter.string(from: item.created_at!))
+                                        .font(.subheadline)
+                                        .fontWeight(.light)
+                                }
+                            }.onDelete(perform: removeNormItem)
+                        }
+                        
+                        Section(header: Text("Low Priority")) {
+                            if low_pri_items.isEmpty {
+                                Text("No Low Priority Items.")
+                                    .foregroundColor(.secondary)
+                            }
+                            
+                            ForEach(low_pri_items, id: \.self) { item in
+                                VStack(alignment: .leading) {
+                                    Text("\(item.title ?? "Unknown")")
+                                    Text(dateFormatter.string(from: item.created_at!))
+                                        .font(.subheadline)
+                                        .fontWeight(.light)
+                                }
+                            }.onDelete(perform: removeLowItem)
+                        }
+                    }
+                    .listStyle(InsetGroupedListStyle())
+                }
+                VStack{
+                    Spacer()
+                    
+                    HStack{
+                        Spacer()
+                        
+                        NavigationLink(destination: NewToDoView(), label: {
+                            Image(systemName: "plus.circle.fill")
+                                .font(.custom("hugeButton", fixedSize: 50))
+                            }
+                        ).padding()
+                    }
+                }
+            }
+            .navigationTitle(Text("Your To-Do's"))
+            .navigationBarTitleDisplayMode(.inline)
+            .onAppear {
+                setup()
+            }
+            
+        }
+    }
+}
+
+struct NewToDoView: View {
+    @Environment(\.managedObjectContext) var managedObjectContext
+    
+    @State var title: String = ""
+    @State var selected: Int = 1
+    
+    @State var isAlertPresented = false
+    @State var alertMessage = ""
+    
+    var body: some View {
+        Form {
+            Section(header: Text("To-Do Title")) {
+                TextField("Type your To-Do Here!", text: $title)
+            }
+            
+            Section(header: Text("To-Do Priority")) {
+                Picker(selection: $selected, label: Text("Priority"), content: {
+                    Text("High").tag(2)
+                    Text("Normal").tag(1)
+                    Text("Low").tag(0)
+                }).pickerStyle(DefaultPickerStyle())
+            }
+            
+            Button(action: {
+                let item = Item(context: managedObjectContext)
+                
+                item.title = title
+                item.priority = (selected == 0 ? "low" : (selected == 2 ? "high" : "normal"))
+                item.created_at = Date()
+                
+                PersistenceController.shared.save()
+                
+                title = ""
+                selected = 1
+                
+                alertMessage = "Successfully Created To-Do!"
+                isAlertPresented.toggle()
+                
+            }, label: {
+                Text("Create New To-Do")
+            }).alert(isPresented: $isAlertPresented, content: {
+                Alert(title: Text("Success!"), message: Text("\(alertMessage)"), dismissButton: .cancel(Text("Ok")))
+            })
+        }
     }
 }
 
--- Tudew/TudewApp.swift
+++ Tudew/TudewApp.swift
@@ -9,9 +9,27 @@ import SwiftUI
 
 @main
 struct TudewApp: App {
+    let persistenceController = PersistenceController.shared
+    
+    @Environment(\.scenePhase) var scenePhase
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }.onChange(of: scenePhase) { (newScenePhase) in
+            switch newScenePhase {
+            
+            case .background:
+                print("Scene is in the Background")
+                persistenceController.save()
+            case .inactive:
+                print("Scene is Inactive")
+            case .active:
+                print("Scene is Active")
+            @unknown default:
+                print("Apple Must Have Changed Something")
+            }
         }
     }
 }
